7f19442ad2de39da7526621c50696d9426e19648
Johannes Haapanen <johannes.haapanen@gmail.com>
2023-04-03 00:07:32 +0300

    Connected client message

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
+use std::sync::Arc;
+
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpListener;
-use tokio::sync::broadcast;
+use tokio::sync::{broadcast, Mutex};
+
+struct ChatClient {
+    addr: String,
+}
+
+struct ChatState {
+    clients: Vec<ChatClient>,
+}
+
+impl ChatClient {
+    fn new(addr: String) -> ChatClient {
+        ChatClient { addr }
+    }
+}
+
+impl ChatState {
+    fn new() -> ChatState {
+        ChatState { clients: vec![] }
+    }
+
+    fn new_client(&mut self, c: ChatClient) {
+        println!("New client: {}", c.addr);
+        self.clients.push(c);
+    }
+}
 
 #[tokio::main]
 async fn main() {
     let address = "localhost:8001";
-
     let (tx, _rx) = broadcast::channel(10);
 
     // Binds the address set above
@@ -13,9 +39,21 @@ async fn main() {
 
     println!("\nStarting the chat server, address: {}", address);
 
+    let state = Arc::new(Mutex::new(ChatState::new()));
+
     loop {
+        let state = Arc::clone(&state);
+
         // Gets the socket and the address of the connected client
         let (mut socket, addr) = listener.accept().await.unwrap();
+
+        // let mut state = state.lock().await;
+
+        // state.new_client(ChatClient::new(addr.to_string().to_string()));
+        //
+        // for client in state.clients.iter() {
+        //     println!("{} -> Connected", client.addr);
+        // }
 
         // Transmitter needs to be cloned because of the ownership
         let tx = tx.clone();
@@ -29,6 +67,11 @@ async fn main() {
 
             let mut reader = BufReader::new(reader);
             let mut line = String::new();
+
+            // Send the new client connected message
+            tx.send((format!("New client connected: {}\n", addr), addr))
+                .unwrap();
+            line.clear();
 
             loop {
                 tokio::select! {
@@ -46,8 +89,7 @@ async fn main() {
 
                         if addr != other_addr {
 
-                            let edited_msg = format!("{}: {}", other_addr, msg);
-                            writer.write_all(edited_msg.as_bytes()).await.unwrap();
+                            writer.write_all(msg.as_bytes()).await.unwrap();
                         }
                     }
 
