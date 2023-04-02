384881ca78239b7a53f03f8fb92216037494f580
Johannes Haapanen <johannes.haapanen@gmail.com>
2023-04-02 23:37:12 +0300

    Tidying up the code

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,29 @@
-use futures::Stream;
-use std::error::Error;
-use std::sync::Arc;
-use std::{collections::HashMap, net::SocketAddr};
-use tokio::io::{AsyncBufReadExt, AsyncRead, AsyncWriteExt, BufReader};
-use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::{broadcast, Mutex};
-
-struct Client {
-    address: String,
-    nickname: String,
-}
-struct ChatState {
-    clients: Vec<Client>,
-}
-
-impl ChatState {
-    fn new() -> ChatState {
-        ChatState { clients: vec![] }
-    }
-}
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::TcpListener;
+use tokio::sync::broadcast;
 
 #[tokio::main]
 async fn main() {
     let address = "localhost:8001";
 
-    let (tx, rx) = broadcast::channel(10);
+    let (tx, _rx) = broadcast::channel(10);
+
+    // Binds the address set above
     let listener = TcpListener::bind(address).await.unwrap();
 
+    println!("\nStarting the chat server, address: {}", address);
+
     loop {
+        // Gets the socket and the address of the connected client
         let (mut socket, addr) = listener.accept().await.unwrap();
+
+        // Transmitter needs to be cloned because of the ownership
         let tx = tx.clone();
+
+        // Receiver is not cloned but got through subscribing to the transmitter
         let mut rx = tx.subscribe();
 
-        // let state = Arc::clone(&state);
-
+        // New thread for each of the clients
         tokio::spawn(async move {
             let (reader, mut writer) = socket.split();
 
@@ -56,7 +46,8 @@ async fn main() {
 
                         if addr != other_addr {
 
-                        writer.write_all(msg.as_bytes()).await.unwrap();
+                            let edited_msg = format!("{}: {}", other_addr, msg);
+                            writer.write_all(edited_msg.as_bytes()).await.unwrap();
                         }
                     }
 
