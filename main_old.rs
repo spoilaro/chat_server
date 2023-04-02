64ae5dad54385f4d4fe8591467f4a7380b4b035c
Johannes Haapanen <johannes.haapanen@gmail.com>
2023-04-02 23:24:09 +0300

    Simple chat server

---
+++ main_old.rs
@@ -0,0 +1,17 @@
+// use tokio::io::{AsyncReadExt, AsyncWriteExt};
+//
+// #[tokio::main]
+// async fn main() {
+//     let listener = tokio::net::TcpListener::bind("localhost:8001")
+//         .await
+//         .unwrap();
+//
+//     let (mut socket, _addr) = listener.accept().await.unwrap();
+//
+//     loop {
+//         let mut buffer = [0u8; 1024];
+//         let bytes_read = socket.read(&mut buffer).await.unwrap();
+//
+//         socket.write_all(&buffer[..bytes_read]).await.unwrap();
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,68 @@
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use futures::Stream;
+use std::error::Error;
+use std::sync::Arc;
+use std::{collections::HashMap, net::SocketAddr};
+use tokio::io::{AsyncBufReadExt, AsyncRead, AsyncWriteExt, BufReader};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::{broadcast, Mutex};
+
+struct Client {
+    address: String,
+    nickname: String,
+}
+struct ChatState {
+    clients: Vec<Client>,
+}
+
+impl ChatState {
+    fn new() -> ChatState {
+        ChatState { clients: vec![] }
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    let listener = tokio::net::TcpListener::bind("localhost:8001")
-        .await
-        .unwrap();
+    let address = "localhost:8001";
 
-    let (mut socket, _addr) = listener.accept().await.unwrap();
+    let (tx, rx) = broadcast::channel(10);
+    let listener = TcpListener::bind(address).await.unwrap();
 
     loop {
-        let mut buffer = [0u8; 1024];
-        let bytes_read = socket.read(&mut buffer).await.unwrap();
+        let (mut socket, addr) = listener.accept().await.unwrap();
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
-        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+        // let state = Arc::clone(&state);
+
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
+            loop {
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                    }
+
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+
+                        if addr != other_addr {
+
+                        writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
+
+
+                }
+            }
+        });
     }
 }
