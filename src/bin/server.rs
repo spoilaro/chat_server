8520a2254011b6871e76bbe416271e156dfab6ac
Johannes Haapanen <johannes.haapanen@gmail.com>
2023-04-07 14:46:42 +0300

    Refactoring

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,33 +1,6 @@
-use std::sync::Arc;
-
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpListener;
-use tokio::sync::{broadcast, Mutex};
-
-struct ChatClient {
-    addr: String,
-}
-
-struct ChatState {
-    clients: Vec<ChatClient>,
-}
-
-impl ChatClient {
-    fn new(addr: String) -> ChatClient {
-        ChatClient { addr }
-    }
-}
-
-impl ChatState {
-    fn new() -> ChatState {
-        ChatState { clients: vec![] }
-    }
-
-    fn new_client(&mut self, c: ChatClient) {
-        println!("New client: {}", c.addr);
-        self.clients.push(c);
-    }
-}
+use tokio::sync::broadcast;
 
 #[tokio::main]
 async fn main() {
@@ -39,21 +12,9 @@ async fn main() {
 
     println!("\nStarting the chat server, address: {}", address);
 
-    let state = Arc::new(Mutex::new(ChatState::new()));
-
     loop {
-        let state = Arc::clone(&state);
-
         // Gets the socket and the address of the connected client
         let (mut socket, addr) = listener.accept().await.unwrap();
-
-        // let mut state = state.lock().await;
-
-        // state.new_client(ChatClient::new(addr.to_string().to_string()));
-        //
-        // for client in state.clients.iter() {
-        //     println!("{} -> Connected", client.addr);
-        // }
 
         // Transmitter needs to be cloned because of the ownership
         let tx = tx.clone();
@@ -79,16 +40,12 @@ async fn main() {
                         if result.unwrap() == 0 {
                             break;
                     }
-
                         tx.send((line.clone(), addr)).unwrap();
                         line.clear();
                     }
-
                     result = rx.recv() => {
                         let (msg, other_addr) = result.unwrap();
-
                         if addr != other_addr {
-
                             writer.write_all(msg.as_bytes()).await.unwrap();
                         }
                     }
